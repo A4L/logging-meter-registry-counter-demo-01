3d3857bb8e876f1a46935e6cd6c8d9ecb4a7b5f4
A4L <billcollector77@gmail.com>
2023-08-09 13:08:08 +0200

    use default configs for meter registries

--- src/test/java/io/a4l/examples/MeterWithLoggingMeterRegistryTest.java
+++ src/test/java/io/a4l/examples/MeterWithLoggingMeterRegistryTest.java
@@ -3,7 +3,6 @@ package io.a4l.examples;
 import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.logging.LoggingMeterRegistry;
 import io.micrometer.core.instrument.logging.LoggingRegistryConfig;
-import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -11,21 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 public class MeterWithLoggingMeterRegistryTest extends AbstractMeterTest {
 
   public MeterWithLoggingMeterRegistryTest() {
-    super(
-        new LoggingMeterRegistry(
-            new LoggingRegistryConfig() {
-
-              @Override
-              public String get(String key) {
-                return null;
-              }
-
-              @Override
-              public Duration step() {
-                return Duration
-                    .ofMillis(1000L);
-              }
-            }, Clock.SYSTEM, log::info),
-        log);
+    super(new LoggingMeterRegistry(LoggingRegistryConfig.DEFAULT, Clock.SYSTEM,
+        log::info), log);
   }
 }
--- src/test/java/io/a4l/examples/MeterWithPrometheusRegistryTest.java
+++ src/test/java/io/a4l/examples/MeterWithPrometheusRegistryTest.java
@@ -2,7 +2,6 @@ package io.a4l.examples;
 
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
-import java.time.Duration;
 import lombok.extern.slf4j.Slf4j;
 
 
@@ -10,21 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 public class MeterWithPrometheusRegistryTest extends AbstractMeterTest {
 
   public MeterWithPrometheusRegistryTest() {
-    super(
-        new PrometheusMeterRegistry(
-            new PrometheusConfig() {
-              @Override
-              public String get(String key) {
-                return null;
-              }
-
-              @Override
-              public Duration step() {
-                return Duration
-                    .ofMillis(1L);
-              }
-
-            }),
-        log);
+    super(new PrometheusMeterRegistry(PrometheusConfig.DEFAULT), log);
   }
 }
